f7c6e187977e4acede2ea09502a0008797ed98b8
minsng <tjdalstmd033@naver.com>
2024-12-26 10:14:41 +0900

    생성

---
+++ src/test/java/com/example/hansol/HansolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hansol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
